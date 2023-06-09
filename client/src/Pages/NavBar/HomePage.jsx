e223144efaeb1584026f178d23cca90b3936569a
fulopdenes <denes.fulopp@gmail.com>
2023-06-09 13:19:00 +0200

    add info for homepage

--- client/src/Pages/NavBar/HomePage.jsx
+++ client/src/Pages/NavBar/HomePage.jsx
@@ -5,7 +5,13 @@ export const HomePage = () => {
     <>
       <div className="HomePage">
         <h1>WELCOME TO OUR WEBSITE</h1>
-        <b>TopMERN</b>
+        <div>
+          This website works with a <b>free</b> server service provider, so the server does not run permanently.
+        </div>
+        <div>
+          To see Employee and Equipment table, after the first request sent you need to wait approximately <b>15 ms</b>, then refresh the current page.
+        </div>
+        <h2>TopMERN</h2>
         <p></p>
         <div>
           We make fun with Employee Data Registration, Equipments and
